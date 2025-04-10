d59a23d3edd58c0a8307fb18c3631b18f3bcb751
Maciej Makowski <maciej.makowski2608@gmail.com>
2025-04-10 16:36:52 +0200

    chore: added mongodb setup

---
+++ backend/app/database.py
@@ -0,0 +1,12 @@
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+def get_mongodb_client():
+    uri = os.getenv("MONGODB_URI")
+    client = MongoClient(uri)
+    return client
+
+db = get_mongodb_client()
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from app.database import db
+
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
+    db.admin.command('ping')
     return {"Hello": "World"}
