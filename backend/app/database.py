db5b5a20d884cb011d9dc1379207d129f9bd2ca7
Maciej Makowski <maciej.makowski2608@gmail.com>
2025-04-13 22:51:26 +0200

    feat: added available tasks endpoint and temporary internal api for adding tasks

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,14 +1,15 @@
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
+import motor.motor_asyncio
 
 load_dotenv()
 
 
 def get_mongodb_client():
     uri = os.getenv("MONGODB_URI")
-    client = MongoClient(uri)
-    return client
+    client = motor.motor_asyncio.AsyncIOMotorClient(uri)
+    return client["bug_hunter_dev"]
 
 
 db = get_mongodb_client()
