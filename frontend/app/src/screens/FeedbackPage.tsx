a0a6109856e28d3f95aecb20abd41d123d0648d2
Krzysztof Śliwiński <krzysztof.sliwinski@swmansion.com>
2025-06-03 20:56:04 +0200

    added feedback page

---
+++ frontend/app/src/screens/FeedbackPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PageContainer from '../components/shared/PageContainer';
+
+const FeedbackPage = () => {
+    return (
+        <PageContainer>
+            
+        </PageContainer>
+    );
+};
+
+export default FeedbackPage;
